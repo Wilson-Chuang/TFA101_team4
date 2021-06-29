61f0cd087c9532e31ec02d992187334043fca3e3
Arifish Chang <arifish0320@gmail.com>
2021-06-29 14:37:38 +0800

    test

--- src/com/shop/model/ShopDAO_interface.java
+++ src/com/shop/model/ShopDAO_interface.java
@@ -5,26 +5,20 @@ import java.util.*;
 public interface ShopDAO_interface {
 	public void insert(ShopVO shopVO);
 
+	public void insert_shop(ShopVO shopVO);
+
 	public void update(ShopVO shopVO);
+
+	public void update_rating(ShopVO shopVO);
 
 	public void delete(Integer shop_id);
 
 	public ShopVO findByPrimaryKey(Integer shop_id);
 	
-	public ShopVO findShop_tax_id(String shop_tax_id);
-	
 	public List<ShopVO> getAll();
 	
-	public List<ShopVO> getAllbyLatLng(Double lat, Double lng);
-	
-	public List<ShopVO> findShopPlace(String place);
-
-	public List<ShopVO> findShopKeyword(String keyword);
-
-	public List<ShopVO> findShopBoth(String keyword, String place);	
-	
-	// ====================組長的code======================
 	public ShopVO findByMemberId(Integer member_id);
 	public void update_shop(ShopVO shopVO);
+	public void add_total_view(Integer shop_id);
 	
 }
